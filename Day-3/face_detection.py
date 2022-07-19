5ff7c989fb2a6fb67f51510a20f09c873b3448bc
Avinash Chourasia <97829396+AvinashChourasia@users.noreply.github.com>
2022-07-19 09:01:40 +0530

    Add files via upload

---
+++ Day-3/face_detection.py
@@ -0,0 +1,17 @@
+import cv2
+alg = "haarcascade_frontalface_default.xml"
+haar_cascade = cv2.CascadeClassifier(alg)
+cam = cv2.VideoCapture(0)
+
+while True:
+    _,img = cam.read()
+    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face = haar_cascade.detectMultiScale(grayImg,1.3,4)
+    for(x,y,w,h) in face:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0),2)
+    cv2.imshow("Face_Detection",img)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
